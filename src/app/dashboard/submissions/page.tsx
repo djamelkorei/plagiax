c72526d806350f38d84dd1b7543b51d76ec9c444
djamelkorei <korei.djamel.eddine@gmail.com>
2025-11-09 19:51:49 +0000

    feat: add new submission dialog with form and file upload functionality

--- src/app/dashboard/submissions/page.tsx
+++ src/app/dashboard/submissions/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import {
+  Alert,
   Avatar,
   Badge,
   Box,
@@ -8,10 +9,21 @@ import {
   ButtonGroup,
   CloseButton,
   Dialog,
+  Field,
+  Fieldset,
+  FileUpload,
   Flex,
+  For,
+  HStack,
+  Icon,
   IconButton,
+  Input,
+  InputGroup,
   Pagination,
   Portal,
+  RadioGroup,
+  Stack,
+  Switch,
   Table,
   Text
 } from "@chakra-ui/react";
@@ -22,7 +34,9 @@ import {SearchFilter} from "@/components/search-filter";
 import moment from "moment";
 import {AiOutlineDelete, AiOutlineFile, AiOutlinePercentage} from "react-icons/ai";
 import {PiFilePdf} from "react-icons/pi";
-import {IoAddOutline} from "react-icons/io5";
+import {IoAddOutline, IoCloudUploadOutline} from "react-icons/io5";
+import {MdArrowRightAlt} from "react-icons/md";
+import {TbSquarePercentage} from "react-icons/tb";
 
 interface SubmissionDTO {
   id: number,
@@ -43,10 +57,35 @@ const dateToFullDateTimeString = (date: string | Date): string => {
     .format('MMM DD, YYYY, HH:mm');
 }
 
+const formSubmissionExclusion = [
+  {
+    field: 'exclusion_bibliographic',
+    label: 'Exclude bibliographic materials'
+  },
+  {
+    field: 'exclusion_quoted',
+    label: 'Exclude quoted materials'
+  },
+  {
+    field: 'exclusion_small_sources',
+    label: 'Exclude small sources (Small match exclusion type)'
+  },
+]
+
+const formSubmissionExclusionThresholdType = [
+  {label: "Words", field: "exclusion_type", value: 'words'},
+  {label: "Percentage", field: "exclusion_type", value: 'percentage'},
+]
+
 export default function DashboardSubmissions() {
 
   const loading = false;
   const [, setTextFilter] = useState<string>('');
+  const [selectedSubmission, setSelectedSubmission] = useState<SubmissionDTO | null>(null);
+
+  const [modelOpen, setModelOpen] = useState(false);
+  const [modelAddOpen, setModelAddOpen] = useState(false);
+
   const submissionList: SubmissionDTO[] = [
     {
       id: 1,
@@ -88,8 +127,6 @@ export default function DashboardSubmissions() {
       aiSimilarityFileUrl: 'https://example.com/ai-similarity3',
     },
   ];
-  const [selectedSubmission, setSelectedSubmission] = useState<SubmissionDTO | null>(null)
-  const [modelOpen, setModelOpen] = useState(false)
 
   const deleteHandler = (item: SubmissionDTO) => {
     setSelectedSubmission(item);
@@ -114,7 +151,9 @@ export default function DashboardSubmissions() {
       <Flex flexDirection={'column'} gap={6}>
 
         <Flex gap={6} alignItems={'center'} flexDirection={{base: 'column', md: 'row'}}>
-          <Button w={'fit-content'}>
+          <Button w={'fit-content'} onClick={() => {
+            setModelAddOpen(true);
+          }}>
             New Submission
             <IoAddOutline/>
           </Button>
@@ -160,7 +199,7 @@ export default function DashboardSubmissions() {
                   borderBottomWidth={submissionList.length - 1 === index ? 0 : 1}
                 >
                   <Table.Cell w={'180px'}>
-                    <Flex gap={4} alignItems={'center'}>
+                    <HStack gap={4}>
                       <Button size={'xs'} variant={'surface'} colorPalette={'blue'}>
                         <AiOutlineFile/>
                       </Button>
@@ -168,10 +207,10 @@ export default function DashboardSubmissions() {
                         <Text>{item.fileName}</Text>
                         <Text textStyle={'xs'} color={'gray.500'}>{dateToFullDateTimeString(item.postedDate)}</Text>
                       </Box>
-                    </Flex>
+                    </HStack>
                   </Table.Cell>
                   <Table.Cell>
-                    <Flex alignItems={'center'} gap={2}>
+                    <HStack alignItems={'center'} gap={2}>
                       <Avatar.Root size={'xs'}>
                         <Avatar.Fallback name={item.studentName}/>
                       </Avatar.Root>
@@ -179,7 +218,7 @@ export default function DashboardSubmissions() {
                         <Text>{item.studentName}</Text>
                         <Text textStyle={'xs'} color={'gray.500'}>{item.studentEmail}</Text>
                       </Box>
-                    </Flex>
+                    </HStack>
                   </Table.Cell>
                   <Table.Cell w={'180px'}>
                     <Badge colorPalette={'teal'}>Completed</Badge>
@@ -290,6 +329,155 @@ export default function DashboardSubmissions() {
                     <Button variant="outline">Cancel</Button>
                   </Dialog.ActionTrigger>
                   <Button variant={'surface'} colorPalette={'red'}>Yes, confirm</Button>
+                </Dialog.Footer>
+                <Dialog.CloseTrigger asChild>
+                  <CloseButton size="sm"/>
+                </Dialog.CloseTrigger>
+              </Dialog.Content>
+            </Dialog.Positioner>
+          </Portal>
+        </Dialog.Root>
+
+
+        <Dialog.Root motionPreset="slide-in-bottom"
+                     size={'xl'}
+                     open={modelAddOpen}
+                     onOpenChange={(d) => setModelAddOpen(d.open)}>
+          <Portal>
+            <Dialog.Backdrop/>
+            <Dialog.Positioner>
+              <Dialog.Content>
+                <Dialog.Header>
+                  <Dialog.Title>Add new submission</Dialog.Title>
+                </Dialog.Header>
+                <Dialog.Body position={'relative'} display={'flex'} flexDirection={'column'} gap={4}>
+                  <Fieldset.Root>
+                    <Stack>
+                      <Fieldset.HelperText>
+                        Please fill out the following fields to submit your document for plagiarism checking. Each field
+                        is important to ensure your document is processed accurately.
+                      </Fieldset.HelperText>
+                    </Stack>
+
+                    <Fieldset.Content display={'flex'} flexDirection={{base: 'column', md: 'row'}} gap={6}>
+
+                      <Flex flexDirection={'column'} gap={4} flex={'1'} position={'relative'}>
+
+                        <Field.Root invalid={false} flex={1}>
+                          <Field.Label>Title</Field.Label>
+                          <Input name="title" type="text" placeholder={'Enter the document title'}/>
+                          <Field.ErrorText>This is an error text</Field.ErrorText>
+                        </Field.Root>
+
+                        <Box mb={3}>
+                          <Text fontWeight={'semibold'} mb={4}>Options ( exclusion )</Text>
+                          <Stack gap={4} align="flex-start">
+                            <For each={formSubmissionExclusion}>
+                              {(item) => (
+                                <Switch.Root key={item.field} size={'sm'} variant={'solid'} cursor={'pointer'}>
+                                  <Switch.HiddenInput name={item.field}/>
+                                  <Switch.Control/>
+                                  <Switch.Label>{item.label}</Switch.Label>
+                                </Switch.Root>
+                              )}
+                            </For>
+                          </Stack>
+                        </Box>
+
+                        <Flex gap={4} flex={1}
+                              flexDirection={'column'}
+                              border={'1px solid'} borderColor={'border'} p={3}
+                              borderRadius={4}>
+                          <Flex justifyContent={'space-between'}>
+                            <Text fontWeight={'semibold'}>
+                              Exclusion Threshold
+                            </Text>
+                            <RadioGroup.Root defaultValue="1" size={'sm'} variant={'outline'}>
+                              <HStack gap={3}>
+                                {formSubmissionExclusionThresholdType.map((item) => (
+                                  <RadioGroup.Item key={item.label} value={item.value} cursor={'pointer'}>
+                                    <RadioGroup.ItemHiddenInput name={item.field}/>
+                                    <RadioGroup.ItemIndicator/>
+                                    <RadioGroup.ItemText>{item.label}</RadioGroup.ItemText>
+                                  </RadioGroup.Item>
+                                ))}
+                              </HStack>
+                            </RadioGroup.Root>
+                          </Flex>
+
+                          <Field.Root>
+                            <Field.Label display={'none'}>Email</Field.Label>
+                            <InputGroup flex={1} startElement={<TbSquarePercentage/>}>
+                              <Input name={'exclusion_value'} size={'xs'} placeholder="Set threshold..."/>
+                            </InputGroup>
+                          </Field.Root>
+
+                        </Flex>
+                      </Flex>
+
+                      <FileUpload.Root
+                        w={{base: 'full', md: '350px'}}
+                        alignItems="stretch"
+                        maxFiles={1}
+                        accept={'.docx,.xlsx,.pptx,.ps,.pdf,.html, .rtf, .odt, .hwp, .txt'}
+                        cursor={'pointer'}
+                        gap={2}
+                      >
+                        <FileUpload.Label>Document</FileUpload.Label>
+                        <FileUpload.HiddenInput/>
+                        <FileUpload.Dropzone minHeight={'auto'}
+                                             height={'-webkit-fill-available'}
+                                             gap={2}
+                                             p={5}
+                                             bg={'gray.50'}
+                                             _hover={{bg: 'orange.50'}}>
+                          <Icon size="xl" color="fg.muted">
+                            <IoCloudUploadOutline/>
+                          </Icon>
+                          <FileUpload.DropzoneContent>
+                            <Box fontSize={'sm'} mb={4}>Drag and drop file here</Box>
+                            <Flex fontSize={'xs'} flexDirection={'column'} color="fg.muted">
+                              <Text as={'span'}>Uploaded file must be less than <b>100 MB</b></Text>
+                              <Text as={'span'}>Uploaded file must has less than <b>800 pages</b></Text>
+                              <Text as={'span'}>Files must contain <b>over 20 words</b> for a similarity report</Text>
+                              <Text as={'span'}>Supported file types for generating repots:</Text>
+                              <Text as={'span'} fontWeight={'bold'}>.docx, .xlsx, .pptx, .ps, .pdf, .html, .rtf, .odt,
+                                .hwp, .txt</Text>
+                            </Flex>
+                          </FileUpload.DropzoneContent>
+                        </FileUpload.Dropzone>
+                        <FileUpload.List clearable/>
+                      </FileUpload.Root>
+                    </Fieldset.Content>
+
+                  </Fieldset.Root>
+
+                  <Alert.Root size={'sm'} variant={'surface'} status={'warning'}>
+                    <Alert.Indicator />
+                    <Alert.Content>
+                      <Alert.Title />
+                      <Alert.Description>
+                        The AI feature is available exclusively for English documents.
+                      </Alert.Description>
+                    </Alert.Content>
+                  </Alert.Root>
+
+                  <Alert.Root size={'sm'} variant={'surface'} status="info" colorPalette="gray">
+                    <Alert.Indicator />
+                    <Alert.Content>
+                      <Alert.Description>
+                        The file you are submitting will not be added to any repository.
+                      </Alert.Description>
+                    </Alert.Content>
+                  </Alert.Root>
+
+
+                </Dialog.Body>
+                <Dialog.Footer>
+                  <Dialog.ActionTrigger asChild>
+                    <Button variant="outline">Cancel</Button>
+                  </Dialog.ActionTrigger>
+                  <Button>Submit <MdArrowRightAlt/></Button>
                 </Dialog.Footer>
                 <Dialog.CloseTrigger asChild>
                   <CloseButton size="sm"/>
